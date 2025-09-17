7c273926e187dbcc012b66cdf111d5900d3f521f
Justin-Bex <justin.bex@cbasyr.org>
2025-09-17 12:04:14 -0400

    More setup

---
+++ Unit1Hw1.java
@@ -0,0 +1,15 @@
+public class Unit1Hw1 {
+
+    public static void main(String[] args) {
+        // TODO
+        // Replace the following lines to write 5-10 lines telling me
+        // about your favorite non-school related hobby.
+        // Use both print and println along with the escape characters (\t and \n)!
+
+        System.out.println("Playing guitar is my favorite non-school hobby");
+        System.out.print("I play the guitar very frequently for fun. \nI have learned \tmany \tmany songs");
+        System.out.print("\nI am always trying to improve \t (even though it is hard sometimes)");
+        System.out.println("\nI also play with my friends who play the drums and play bass");
+        System.out.println("Overall, playing guitar is my favorite non-school hobby. \nI'd recommend everyone to give it a try sometime");
+    }
+}
