e23f8d9967e0d197eb653f44483d472b9fcbc543
Justin-Bex <justin.bex@cbasyr.org>
2025-10-07 14:05:27 -0400

    Update Unit1Hw4.java

--- unit1_homework/Unit1Hw4.java
+++ unit1_homework/Unit1Hw4.java
@@ -13,22 +13,24 @@ public class Unit1Hw4 {
         System.out.println("\n\n");
 
         // TODO: INDEXOF - takes String as parameter
-        System.out.println(s1.indexOf("o"));
+        System.out.println(s1.indexOf("o")); //Returns the index of the of the first instance of the character or substring
         System.out.println(s1.indexOf("m"));
         System.out.println(s1.indexOf("o"));
         System.out.println(s1.indexOf("c"));
         System.out.println(s1.indexOf("r"));
-        // TODO: SUBSTRING - with one parameter
+        // TODO: SUBSTRING - with one parameter //Returns the string but only the part after the value (value is the index of the characters)
         System.out.println(s1.substring(4));
+        System.out.println(s1.substring(1));
         // TODO: SUBSTRING - with two parameters
-        System.out.println(s1.substring(0, 4));
+        System.out.println(s1.substring(0, 4)); //Returns the string but only the characters between the 2 values (value is the index of the characters)
+        System.out.println(s1.substring(4, 7));
         // TODO: EQUALS
         String s2 = "Peanut";
         String s3 = "computer";
-        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s2)); //Checks if the strings are equal (outputs boolean)
         System.out.println(s1.equals(s3));
         // TODO: COMPARETO
-        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s2)); //Compares the strings lexicographically
         System.out.println(s1.compareTo(s3));
     }
 
